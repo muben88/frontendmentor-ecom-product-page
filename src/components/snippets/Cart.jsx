9a97f2220c1d57fb92e7f60b0f66491cdee4c87f
muben88 <ga4tech@gmail.com>
2022-04-21 16:32:45 +0000

    create cart component

---
+++ src/components/snippets/Cart.jsx
@@ -0,0 +1,13 @@
+function Cart() {
+  return (
+    <div className="cart-container">
+      <div className="cart-title">
+        <p>Cart</p>
+      </div>
+      <div className="cart-items">
+        <p>Your cart is empty</p>
+      </div>
+    </div>
+  );
+}
+export default Cart;
